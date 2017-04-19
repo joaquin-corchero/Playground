1801aaa4fcf0ffcd5b1f2f1e42f372fe7202cf6f
Joaquin Corchero <joaquin.corchero@gmail.com>
2017-04-19 22:36:52 +0100

    started with backward propagation

--- machine-learning/machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning/machine-learning-ex4/ex4/nnCostFunction.m
@@ -62,24 +62,29 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+%Forward propagation
 a1 = [ones(m, 1) X];
 z2 = a1 * Theta1';
 a2 = [ones(m, 1) sigmoid(z2)];
 a3 = sigmoid(a2 * Theta2');
 h = a3;
 
+%Cost function regularized
 y_matrix = eye(num_labels)(y,:);
-
 cost = (-y_matrix .* log(h)) .- ((1 - y_matrix) .* log(1 - h));
-
-
 JUnreg = (sum(sum(cost)) *  1/m);
-
 theta1 = Theta1(:, 2:end);
 theta2 = Theta2(:, 2:end);
 reg = ((sum(sum(theta1 .^ 2))) + (sum(sum(theta2 .^ 2)))) * (lambda/(2 * m));
-
 J = JUnreg + reg;
+
+%Backward propagation
+A = ones(t, 0);
+for t = 1:m
+   A(t) = X(t);
+endfor
+
+
 
 % -------------------------------------------------------------
 
