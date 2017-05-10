af621fa5f84adc7d528a5819b1d9aee1ea3a1e41
JoaquinCorcheroFMP <jcorchero@findmypast.com>
2017-05-10 08:26:46 +0100

    Selecting the threshold

--- machine-learning/machine-learning-ex8/ex8/selectThreshold.m
+++ machine-learning/machine-learning-ex8/ex8/selectThreshold.m
@@ -22,19 +22,17 @@ for epsilon = min(pval):stepsize:max(pval)
     %               
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
+    
+    predictions = (pval < epsilon);
+    truePositives = sum((predictions == 1) & (yval == 1));
+    falsePositives = sum((predictions == 1) & (yval == 0));
+    falseNegatives = sum((predictions == 0) & (yval == 1));
 
-
-
-
-
-
-
-
-
-
-
-
-
+    precission = truePositives / (truePositives + falsePositives);
+    recall = truePositives/(truePositives + falseNegatives);
+    
+    F1 = (2 * precission * recall)/(precission + recall);
+    
     % =============================================================
 
     if F1 > bestF1
